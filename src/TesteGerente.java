f05952241341c7beffed869d15cfd84d9253453e
guijun13 <guilhermejun13.grillo@gmail.com>
2021-06-15 15:24:09 -0300

    :sparkles: Add shared method using polymorphism
    Method 'registra' uses getBonificacao based on user class called on main

--- src/TesteGerente.java
+++ src/TesteGerente.java
@@ -7,15 +7,32 @@ public class TesteGerente {
 		g1.setCpf("9039021212-1");
 		g1.setSalario(9000);
 		
-		System.out.println(g1.getNome());
-		System.out.println(g1.getCpf());
-		System.out.println(g1.getSalario());
+//		System.out.println(g1.getNome());
+//		System.out.println(g1.getCpf());
+//		System.out.println(g1.getSalario());
 		
 		g1.setSenha(2222);
 		boolean autenticou = g1.autentica(2222);
-		System.out.println(autenticou);
+//		System.out.println(autenticou);
 		
-		System.out.println(g1.getBonificacao());
+//		System.out.println(g1.getBonificacao());
+		
+		// ---------------------------------------------
+		
+		Funcionario g2 = new Gerente();
+		g2.setNome("Marcos o Gerente 2");
+		g2.setCpf("9039021212-1");
+		g2.setSalario(9000);
+		
+		Funcionario f = new Funcionario();
+		f.setSalario(2500);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		
+		System.out.println(controle.getSoma());
+		
 	}
 
 }
